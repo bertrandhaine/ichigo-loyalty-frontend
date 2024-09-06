51e974568419577537d67ab1ee3cec9063b9342e
Bertrand <bertrand.haine@vizeurope.com>
2024-09-06 19:02:03 +0900

    add simple homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CustomerTierPage from "./components/CustomerTierPage";
 import OrderHistoryPage from "./components/OrderHistoryPage";
+import HomePage from "./components/HomePage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/customer/:id/tier" element={<CustomerTierPage />} />
         <Route
           path="/customer/:id/order-history"
---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/HomePage.css";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Welcome to Our Application</h1>
+      <p className="home-subtitle">Here are the available routes:</p>
+      <ul className="home-list">
+        <li className="home-list-item">
+          <Link to="/customer/:id/tier" className="home-link">
+            Customer Tier Page
+          </Link>
+          <span className="home-description">
+            {" "}
+            - Replace :id with the customer ID
+          </span>
+        </li>
+        <li className="home-list-item">
+          <Link to="/customer/:id/order-history" className="home-link">
+            Order History
+          </Link>
+          <span className="home-description">
+            {" "}
+            - Replace :id with the customer ID
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
